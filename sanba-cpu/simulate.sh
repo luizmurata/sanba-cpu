e4c8a47262f366117377777ee06f5e10a8f04564
luizmurata <luizjunior.murata@studenti.unitn.it>
2022-07-15 23:30:53 +0200

    Small fix on the compile&run script

--- sanba-cpu/simulate.sh
+++ sanba-cpu/simulate.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 # for why I'm using --ieee=synopsys --std=c93c -fexplicit: http://ghdl.free.fr/ghdl/IEEE-library-pitfalls.html
-echo $(pwd)
-for f in $(ls ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/*.vhd)
-do
-    echo "$f"
-    ghdl -a --ieee=synopsys --std=93c -fexplicit $f
-done
+ghdl -a --ieee=synopsys --std=93c -fexplicit ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/vga.vhd
+ghdl -a --ieee=synopsys --std=93c -fexplicit ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/videounit.vhd
+ghdl -a --ieee=synopsys --std=93c -fexplicit ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/memory.vhd
+ghdl -a --ieee=synopsys --std=93c -fexplicit ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/cpu.vhd
+ghdl -a --ieee=synopsys --std=93c -fexplicit ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/cpu_tb.vhd
+ghdl -a --ieee=synopsys --std=93c -fexplicit ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/sanbacpu.vhd
 ghdl -e $1 && ghdl -r $1
