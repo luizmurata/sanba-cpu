e73179a75d181f53612a55c925aedeefcdc4b1fe
luizmurata <luizjunior.murata@studenti.unitn.it>
2022-07-16 22:19:36 +0200

    Now the simulate.sh script should work

--- sanba-cpu/simulate.sh
+++ sanba-cpu/simulate.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
-# for why I'm using --ieee=synopsys --std=c93c -fexplicit: http://ghdl.free.fr/ghdl/IEEE-library-pitfalls.html
-ghdl -a --ieee=synopsys --std=93c -fexplicit ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/vga.vhd
-ghdl -a --ieee=synopsys --std=93c -fexplicit ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/videounit.vhd
-ghdl -a --ieee=synopsys --std=93c -fexplicit ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/memory.vhd
-ghdl -a --ieee=synopsys --std=93c -fexplicit ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/cpu.vhd
-ghdl -a --ieee=synopsys --std=93c -fexplicit ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/cpu_tb.vhd
-ghdl -a --ieee=synopsys --std=93c -fexplicit ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/sanbacpu.vhd
-ghdl -e $1 && ghdl -r $1
+# for why I'm using -fsynopsys: http://ghdl.free.fr/ghdl/IEEE-library-pitfalls.html
+# install ghdl dev because otherwise it will complain
+ghdl -a --std=08 -fsynopsys ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/vga.vhd
+ghdl -a --std=08 -fsynopsys ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/videounit.vhd
+ghdl -a --std=08 -fsynopsys ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/memory.vhd
+ghdl -a --std=08 -fsynopsys ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/cpu.vhd
+ghdl -a --std=08 -fsynopsys ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/cpu_tb.vhd
+ghdl -a --std=08 -fsynopsys ./sanba-cpu/sanba-cpu\.srcs/sources_1/new/sanbacpu.vhd
+ghdl -e -fsynopsys $1 && ghdl -r -fsynopsys $1
